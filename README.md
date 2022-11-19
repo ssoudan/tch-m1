77bdbd549d648e5cb8b6e93770c71ca52f9113c1
Sebastien Soudan <sebastien.soudan@gmail.com>
2022-11-18 18:31:44 -0800

    [+] update to pytorch 1.13 and tch-rs git HEAD with MPS device support

--- README.md
+++ README.md
@@ -1,19 +1,15 @@
 # Demo of tch-rs on M1
 
-This repo demonstrate how to use [LaurentMazare/tch-rs](https://github.com/LaurentMazare/tch-rs) on M1. 
+This repo demonstrate how to use [LaurentMazare/tch-rs](https://github.com/LaurentMazare/tch-rs) on M1.
 
 ## Steps to reproduce
 
-- install miniforge with homebrew -- See  https://naolin.medium.com/conda-on-m1-mac-with-miniforge-bbc4e3924f2b
-- create a new conda environment: `conda create -n tch-rs-demo python=3.8`
+- install miniforge with homebrew -- See https://naolin.medium.com/conda-on-m1-mac-with-miniforge-bbc4e3924f2b
+- create a new conda environment: `conda env create -f environment.yml`
 - activate the new environment: `conda activate tch-rs-demo`
-- install pytorch: `conda install pytorch`
-- create a symlink in this repo: `ln -s /opt/homebrew/Caskroom/miniforge/base/envs/tch-rs-demo/lib/python3.8/site-packages/torch/ torch`
+- create a symlink in this repo: `ln -sf /opt/homebrew/Caskroom/miniforge/base/envs/tch-rs-demo/lib/python3.8/site-packages/torch/ torch`
 - run: `cargo run`
 
 ## More
 
 - .cargo/config.toml is used to set envs for torch-sys to find the library and headers.
-- Tensor::print() segfaults.
-- Not tried (yet) with the Metal version of pytorch.
-
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,29 @@
 use tch::{kind, Tensor};
 
 fn grad_example() {
-    let mut x = Tensor::from(2.0).set_requires_grad(true);
+    let mut x = Tensor::from(2.0f32)
+        .to_device(tch::Device::Mps)
+        .set_requires_grad(true);
     let y = &x * &x + &x + 36;
-    println!("{}", y.double_value(&[]));
+    println!("y {}", y.double_value(&[]));
+
     x.zero_grad();
     y.backward();
+
     let dy_over_dx = x.grad();
-    println!("{}", dy_over_dx.double_value(&[]));
+    println!("dy/dx {}", dy_over_dx.double_value(&[]))
 }
 
 fn main() {
-    tch::maybe_init_cuda();
     let t = Tensor::of_slice(&[3, 1, 4, 1, 5]);
-    // t.print(); // For some reason t.print() segfaults
-    println!("{:?}", &t);
-    let t = Tensor::randn(&[5, 4], kind::FLOAT_CPU);
-    println!("{:?}", &t);
+    t.print(); // works on CPU tensors
+
+    println!("t(cpu) {:?}", &t);
+    println!("t device: {:?}", &t.device());
+    let t = Tensor::randn(&[5, 4], kind::FLOAT_CPU).to_device(tch::Device::Mps);
+    // t.print(); - panics with "Cannot convert a MPS Tensor to float64 dtype as the MPS framework doesn't support float64. Please use float32 instead..."
+    println!("t(mps) {:?}", &t);
+    println!("t device: {:?}", &t.device());
+
     grad_example();
-    println!("Cuda available: {}", tch::Cuda::is_available());
-    println!("Cudnn available: {}", tch::Cuda::cudnn_is_available());
 }
